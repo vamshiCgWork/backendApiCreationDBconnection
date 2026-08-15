ab63caf37471f41400af111ca25a76fa830e45d8
vamshi netha <v-vamsshinetha.adepu@shutterfly.com>
2026-08-15 15:14:02 +0530

    updated apis and connected to db

--- src/app.js
+++ src/app.js
@@ -1,43 +1,9 @@
 import express from 'express'
-
+import postRoutes from './routers/post.routers.js'
 const app = express()
 app.use(express.json())
-const notes =[]
 
-app.post("/createNotes",(req,res)=>{
-    const note = req.body
-    notes.push(note)
-    res.status(201).json({
-        message:"notes created",
-        notes:notes
-    })
-})
 
-app.get("/allNotes",(req,res)=>{
-    res.status(200 ).json({
-        message:"fetched all notes",
-        notes:notes
-    })
-})
-
-app.delete("/notes/:index",(req,res)=>{
-const index = req.params.index
-delete notes[index]
-res.status(200).json({
-    message:"note deleted"
-})
-})
-
-app.patch("/notes/:index", (req,res)=>{
-    const index = req.params.index
-    const content = req.body.content
-
-    notes[index].content = content
-
-    res.status(201).json({
-        message:"upated",
-        note:notes[index]
-    })
-})
+app.use("/api",postRoutes)
 
 export default app
