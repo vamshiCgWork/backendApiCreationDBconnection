7f72be146b6cefe3ba16d2534981d690516a6967
vamshi netha <v-vamsshinetha.adepu@shutterfly.com>
2026-08-20 20:57:47 +0530

    feat: add likes and isLiked fields to post model and update creation logic in controller

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
-    image:String,
-    description:String
-})
+  image: String,
+  description: String,
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  isLiked: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-const postModel = mongoose.model("post",postSchema)
+const postModel = mongoose.model("post", postSchema);
 
-export default postModel
+export default postModel;
